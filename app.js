a4138f5a8a8a28a62a771d9c636f9ec078e983f3
Grant <grant.bradshaw@outlook.com>
2016-04-22 11:31:28 -0700

    Updating db connection

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(session({
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
   store: new MongoStore({
-    url: process.env.MONGODB || process.env.MONGOLAB_URI,
+    url: process.env.MONGODB || process.env.MONGOLAB_URI || process.env.MONGODB_URI,
     autoReconnect: true
   })
 }));
